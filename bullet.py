efdfc483a6fc0eea26c20f2db1cb81920361ab8b
ping <wangyp828@126.com>
2016-12-25 13:52:41 +0800

    add bullet

---
+++ bullet.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+import pygame
+import sys
+import traceback 
+from random import *
+from pygame.locals import *
+
+#===========普通子弹==============
+class Bullet1(pygame.sprite.Sprite):
+	"""docstring for Bullet1"""
+	def __init__(self, position):
+		pygame.sprite.Sprite.__init__(self)
+		self.image=pygame.image.load("image/bullet1.png")
+		self.rect=self.image.get_rect()
+		self.rect.left,self.rect.top=position
+		self.speed=12
+		self.active=True
+		self.mask=pygame.mask.from_surface(self.image)
+
+	def move(self):
+		if self.rect.top<0:
+			self.active=False
+		else:
+			self.rect.top-=self.speed
+
+	def reset(self,position):
+		self.rect.left,self.rect.top=position
+		self.active=True
+
+
+		
